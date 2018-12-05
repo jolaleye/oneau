69952ce7027f301d22499104cca8411146982c5e
Jonathan <j.olaleye.o@gmail.com>
2018-12-05 17:56:04 -0600

    Extend Earth class from three.js mesh

--- js/earth.js
+++ js/earth.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three';
 
-class Earth {
-  constructor() {}
+class Earth extends THREE.Mesh {
+  constructor() {
+    // pass a geometry and material to the parent Mesh class
+    super(new THREE.SphereBufferGeometry(10, 20, 20), new THREE.MeshBasicMaterial({ color: '#62A9FF' }));
+  }
 }
 
 export default Earth;
