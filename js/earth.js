2af77322412ee0f6a9a0714a26d720d36331fb08
Jonathan <j.olaleye.o@gmail.com>
2018-12-07 22:00:04 -0600

    Move Sun to origin instead of Earth (and update pov position accordingly)

--- js/earth.js
+++ js/earth.js
@@ -5,6 +5,8 @@ import _ from '../settings.json';
 class Earth extends THREE.Mesh {
   constructor() {
     super(new THREE.SphereBufferGeometry(_.earth.radius, 100, 100), new THREE.MeshBasicMaterial({ color: '#62A9FF' }));
+
+    this.position.z = _.earth.orbitRadius;
   }
 }
 
--- js/sun.js
+++ js/sun.js
@@ -5,8 +5,6 @@ import _ from '../settings.json';
 class Sun extends THREE.Mesh {
   constructor() {
     super(new THREE.SphereBufferGeometry(_.sun.radius, 100, 100), new THREE.MeshBasicMaterial({ color: '#FFBF62' }));
-
-    this.position.set(0, 0, -_.earth.orbit.radius);
   }
 }
 
