742bc0eb8429ad3643b7aa4901208d5bb81b1b00
Jonathan <j.olaleye.o@gmail.com>
2018-12-08 15:54:19 -0600

    Rename some settings

--- js/earth.js
+++ js/earth.js
@@ -4,9 +4,12 @@ import _ from '../settings.json';
 
 class Earth extends THREE.Mesh {
   constructor() {
-    super(new THREE.SphereBufferGeometry(_.earth.radius, 100, 100), new THREE.MeshBasicMaterial({ color: '#62A9FF' }));
+    super(
+      new THREE.SphereBufferGeometry(_.earth.radius, _.earth.segments, _.earth.segments),
+      new THREE.MeshBasicMaterial({ color: '#62A9FF' })
+    );
 
-    this.position.z = _.earth.orbitRadius;
+    this.position.z = _.earth.orbit;
   }
 }
 
