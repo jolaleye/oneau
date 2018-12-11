89d1a44a019648e11f95fdd1bca9b68a5e4da80b
Jonathan <j.olaleye.o@gmail.com>
2018-12-10 19:54:15 -0600

    Simplify phase handling

--- js/core.js
+++ js/core.js
@@ -10,17 +10,15 @@ class Core {
     this.scene = scene;
     this.renderer = renderer;
 
-    // set up scene objects
-    this.pov = new POV();
     this.milkyway = new MilkyWay();
     this.sun = new Sun();
     this.earth = new Earth();
     this.scene.add(this.milkyway, this.sun, this.earth);
 
-    // current scene phase
+    this.pov = new POV();
+
     this.phase = '';
 
-    // start the "wait" phase
     this.startWait();
   }
 
@@ -34,15 +32,8 @@ class Core {
     const ts = (performance.now() - this.lastTick) / 1000; // scales speeds to be 1 u/s regardless of frame rate
     this.lastTick = performance.now();
 
-    let phaseData = {};
-    switch (this.phase) {
-      case 'intro':
-        phaseData = this.getIntroData();
-        break;
-    }
-
-    this.pov.update(ts, this.phase, phaseData);
-    this.earth.update(ts, this.phase, phaseData);
+    this.pov.update(ts, this.phase);
+    this.earth.update(ts, this.phase);
 
     this.renderer.render(this.scene, this.pov.camera);
   }
@@ -52,39 +43,22 @@ class Core {
   startWait() {
     this.phase = 'wait';
     this.earth.orbit.add(this.pov.camera);
-    this.pov.camera.position.set(0, _.earth.orbitHeight, 0);
-    this.pov.camera.rotation.set(0, -Math.PI / 2, 0);
-    this.pov.desiredRotation.set(0, -Math.PI / 2, 0);
+    this.pov.camera.position.z = _.earth.orbitalHeight;
+    this.pov.camera.rotation.set(Math.PI / 2.5, 0, 0);
+    this.pov.desiredRotation.set(Math.PI / 2.5, 0, 0);
   }
 
   // intro phase runs after the user has clicked to start
   // - orbit rotates to bring camera down
-  // - pov rotates to counteract rotating orbit
   startIntro() {
     this.phase = 'intro';
+    this.earth.events.once('introDone', this.startAU.bind(this));
     this.pov.lock();
-    this.pov.camera.rotation.set(-Math.PI / 8, -Math.PI / 2, 0);
-    this.pov.desiredRotation.set(Math.PI / 2, -Math.PI / 2, 0);
-  }
-  getIntroData() {
-    // degrees left to rotate
-    const delta = this.earth.orbit.rotation.z - -Math.PI / 2;
-    const rotationComplete = delta < 0.001;
-    if (rotationComplete) this.startAU();
-
-    const orbitSpeed = Math.max(delta, 0.75) * _.earth.orbitIntroSpeed;
-
-    return { orbitSpeed };
   }
 
   // AU phase runs when the camera has been orbited into place and is ready to travel
   startAU() {
     this.phase = 'au';
-    this.pov.locked = false;
-    this.earth.orbit.remove(this.pov.camera);
-    this.pov.camera.position.set(0, 0, _.earth.sunOrbitRadius - _.earth.orbitHeight);
-    this.pov.camera.rotation.set(0, 0, 0);
-    this.pov.desiredRotation.set(0, 0, 0);
   }
 }
 
