fdc89369fc5a954a51f7285c8d5b33addbd468d4
Jonathan <j.olaleye.o@gmail.com>
2018-12-15 09:51:16 -0600

    Add point light

--- js/sun.js
+++ js/sun.js
@@ -8,6 +8,8 @@ class Sun extends THREE.Mesh {
       new THREE.SphereBufferGeometry(_.sun.radius, _.sun.segments, _.sun.segments),
       new THREE.MeshBasicMaterial({ color: '#FFBF62' })
     );
+
+    this.add(new THREE.PointLight());
   }
 }
 
