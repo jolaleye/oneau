30278a0e6e7125cb594d57603bae97467223ae98
Jonathan <j.olaleye.o@gmail.com>
2018-12-23 11:00:35 -0600

    Add earth class

---
+++ js/Earth.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+
+import _ from '../settings.json';
+
+class Earth extends THREE.Mesh {
+  constructor() {
+    super(
+      new THREE.SphereBufferGeometry(_.earth.radius, _.earth.segments, _.earth.segments),
+      new THREE.MeshBasicMaterial({ color: '#68D8FF' })
+    );
+
+    this.position.z = _.earth.orbitRadius;
+  }
+}
+
+export default Earth;
