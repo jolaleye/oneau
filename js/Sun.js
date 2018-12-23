d22e27bac03f954b745554679582679d4690cd36
Jonathan <j.olaleye.o@gmail.com>
2018-12-22 18:35:04 -0600

    Add sun class

---
+++ js/Sun.js
@@ -0,0 +1,17 @@
+import * as THREE from 'three';
+
+import _ from '../settings.json';
+
+class Sun extends THREE.Mesh {
+  constructor() {
+    super(
+      new THREE.SphereBufferGeometry(_.sun.radius, _.sun.segments, _.sun.segments),
+      new THREE.MeshBasicMaterial({ color: '#FFBF62' })
+    );
+
+    // sunlight
+    this.add(new THREE.PointLight());
+  }
+}
+
+export default Sun;
