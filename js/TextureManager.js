f3d7f5ce629e58ab2887da0ceaa922132e299eaa
Jonathan <j.olaleye.o@gmail.com>
2018-12-24 10:16:55 -0600

    Add galaxy texture

--- js/TextureManager.js
+++ js/TextureManager.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 
+import galaxy from '../img/galaxy.jpg';
+
 import earth from '../img/earth.jpg';
 import earthElev from '../img/earth-elev.jpg';
 import earthWater from '../img/earth-water.png';
@@ -12,6 +14,8 @@ class TextureManager {
 
   load() {
     return new Promise((resolve, reject) => {
+      this.galaxy = this.loader.load(galaxy, undefined, undefined, reject);
+
       this.earth = this.loader.load(earth, undefined, undefined, reject);
       this.earthElev = this.loader.load(earthElev, undefined, undefined, reject);
       this.earthWater = this.loader.load(earthWater, undefined, undefined, reject);
