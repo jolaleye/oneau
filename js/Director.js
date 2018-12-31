9a3b40587caafcfced6b733cc87d786326f5ec29
Jonathan <j.olaleye.o@gmail.com>
2018-12-31 14:57:42 -0600

    Add subtitle interface & Josefin font

--- js/Director.js
+++ js/Director.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import TWEEN from '@tweenjs/tween.js';
+import EventEmitter from 'events';
 
 import _ from '../settings.json';
 
@@ -9,6 +10,30 @@ class Director {
     this.pov = pov;
     this.sun = sun;
     this.earth = earth;
+  }
+
+  overlayText(text, fadeDuration, duration) {
+    const el = document.createElement('p');
+    el.appendChild(document.createTextNode(text));
+    el.classList.add('text');
+    document.querySelector('.overlay').appendChild(el);
+
+    const e = new EventEmitter();
+
+    const fadeIn = new TWEEN.Tween({ opacity: 0 })
+      .to({ opacity: 1 }, fadeDuration)
+      .easing(TWEEN.Easing.Quintic.Out)
+      .onUpdate(({ opacity }) => (el.style.opacity = opacity));
+
+    const fadeOut = new TWEEN.Tween({ opacity: 1 })
+      .to({ opacity: 0 }, fadeDuration)
+      .easing(TWEEN.Easing.Quintic.Out)
+      .onUpdate(({ opacity }) => (el.style.opacity = opacity))
+      .delay(duration)
+      .onComplete(() => e.emit('done'));
+
+    fadeIn.chain(fadeOut).start();
+    return e;
   }
 
   // WAIT phase
