d2ed16bd78481a904103f8fcc135dda072f69ccc
Jonathan <j.olaleye.o@gmail.com>
2019-01-12 17:58:36 -0600

    Add unit to mile conversion

--- js/utils.js
+++ js/utils.js
@@ -3,3 +3,5 @@ export const km2u = km => km / 100000;
 export const u2km = u => u * 100000;
 
 export const clamp = (value, min, max) => Math.max(Math.min(value, max), min);
+
+export const u2mi = u => u * 62137.119;
